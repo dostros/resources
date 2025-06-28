4414a68414bef7673d578e81c6f10c5b703dc67c
dostros <danalieth711@hotmail.fr>
2025-06-28 13:07:44 +0200

    PUBLICSERVICE TABLET

---
+++ [PIPOURP]/d-PublicService/client.lua
@@ -0,0 +1,28 @@
+
+RegisterCommand("ems", function()
+    SetNuiFocus(true, true)
+    SendNUIMessage({
+        action = "open"
+    })
+end)
+
+
+
+
+
+
+
+
+RegisterNUICallback("getemployeelist", function()
+
+
+
+
+
+
+
+
+
+
+
+end)
